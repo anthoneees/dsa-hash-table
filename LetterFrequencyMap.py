9e083804f01dac4c4136dccf4c0530765f260290
Anthony <anthon.ee.es@gmail.com>
2025-10-19 13:43:19 -0400

    Ask for user input for file directory

--- LetterFrequencyMap.py
+++ LetterFrequencyMap.py
@@ -3,9 +3,19 @@ from HashTable import Table
 
 
 def main():
+    user_path = input("Please enter the full path to the file \n")
+
+    if os.path.exists(user_path):
+        if not os.path.isfile(user_path):
+            print("The path is not a file")
+            return
+    else:
+        print(f"The path '{user_path}' does not exist.")
+        return
+
     try:
         ht = Table(capacity=19)
-        with open("rfc791.txt", "r") as f:
+        with open(user_path, "r") as f:
             for line in f:
                 for char in line:
                     if not char.isalpha():
@@ -20,11 +30,11 @@ def main():
         ht.printValues()
 
     except IOError as e:
-        print(f"Failed to open or write to rfc919.txt: {e}")
+        print(f"Failed to open or write: {e}")
         # You can add more specific error handling here based on 'e'
         # For example, checking for permission errors:
         if not os.access("rfc919.txt", os.W_OK):
-            print("Permission denied to write to rfc919.txt")
+            print("Permission denied to write")
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
